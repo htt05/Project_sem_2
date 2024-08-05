a7bb51381da7038570b3f5c6369fbe5d6daf54e2
htt05 <zxcvbnm05082003@gmail.com>
2024-08-05 15:14:48 +0700

    scas

---
+++ src/main/java/entities/Blog.java
@@ -0,0 +1,22 @@
+package entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Blogs")
+public class Blog  implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private 
+}
---
+++ src/main/java/entities/CatBlog.java
@@ -0,0 +1,35 @@
+package entities;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotEmpty;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+@Table(name = "CatBlogs")
+public class CatBlog implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	@NotEmpty(message = "Tên danh mục không được để trống")
+	private String title;
+	private boolean status;
+	@OneToMany(mappedBy = "category", fetch = FetchType.EAGER)
+	private List<> products;
+}
